be75dd41d4ff53d27f3eb714cf160de0af2140dc
Tjeri <tjeri@hadiko.de>
2022-12-25 11:52:50 +0100

    extend point
    - add mul + mod
    - better hash
    - get_neighbors function

--- aoc/coord2d/point.py
+++ aoc/coord2d/point.py
@@ -29,11 +29,30 @@ class Point:
         self.y -= other.y
         return self
 
+    def __mul__(self, times: int) -> Point:
+        return Point(times * self.x, times * self.y)
+
+    def __imul__(self, times: int) -> Point:
+        self.x *= times
+        self.y *= times
+        return self
+
+    def __mod__(self, other: Point) -> Point:
+        return Point(self.x % other.x, self.y % other.y)
+
+    def __imod__(self, other: Point) -> Point:
+        self.x %= other.x
+        self.y %= other.y
+        return self
+
     def __copy__(self) -> Point:
         return Point(self.x, self.y)
 
     def __hash__(self) -> int:
-        return hash(f'{self.x}/{self.y}')
+        return hash((self.x, self.y))
 
     def manhattan_distance(self, other: Point) -> int:
         return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def get_neighbors(self) -> list[Point]:
+        return [self + Point(0, -1), self + Point(1, 0), self + Point(0, 1), self + Point(-1, 0)]
