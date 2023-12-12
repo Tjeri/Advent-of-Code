4f10c0635553eb757f157c43f350ca150d060713
Tjeri <tjeri92@gmail.com>
2023-12-12 11:29:09 +0100

    2023/12: use @cache

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,24 +1,21 @@
+from functools import cache
+
 from aoc.input import read_input
 
 
-def parse_line(line: str) -> tuple[str, list[int]]:
+def parse_line(line: str) -> tuple[str, tuple[int, ...]]:
     records, groups = line.split(' ')
-    return records, list(map(int, groups.split(',')))
+    return records, tuple(map(int, groups.split(',')))
 
 
-def find_arrangements(records: str, groups: list[int], history: dict[str, int] | None = None) -> int:
-    if history is None:
-        history = {}
+@cache
+def find_arrangements(records: str, groups: tuple[int, ...]) -> int:
     if len(groups) == 0:
         return 1
     arrangements = 0
     total_size = sum(groups) + len(groups) - 1
     size = groups[0]
     for i in range(len(records) - total_size + 1):
-        key = records[i:] + ':' + ','.join(map(str, groups))
-        if key in history:
-            arrangements += history[key]
-            continue
         if any(records[j] == '#' for j in range(0, i)):
             break
         if len(groups) == 1 and any(records[j] == '#' for j in range(i + size, len(records))):
@@ -28,9 +25,7 @@ def find_arrangements(records: str, groups: list[int], history: dict[str, int] |
         if (all(j < len(records) and records[j] != '.' for j in range(i, i + size))
                 and (i + size == len(records) or records[i + size] != '#')
                 and (i == 0 or records[i - 1] != '#')):
-            _arrangements = find_arrangements(records[i + size + 1:], groups[1:], history)
-            history[key] = _arrangements
-            arrangements += _arrangements
+            arrangements += find_arrangements(records[i + size + 1:], groups[1:])
     return arrangements
 
 
