909702f05f526938d95dfaae395a9bd6a2a23fee
Tjeri <tjeri92@gmail.com>
2023-12-14 19:33:44 +0100

    2023/08

---
+++ 2023/day08.py
@@ -0,0 +1,41 @@
+from itertools import cycle
+from math import lcm
+
+from parse import parse
+
+from aoc.input import read_input
+
+map_direction = {'R': 1, 'L': 0}
+map_pattern = '{} = ({}, {})'
+
+
+def parse_directions(lines: list[str]) -> cycle:
+    return cycle([map_direction[direction] for direction in lines[0]])
+
+
+def parse_map(lines: list[str]) -> dict[str, tuple[str, str]]:
+    result = dict()
+    for line in lines[2:]:
+        node, left, right = parse(map_pattern, line)
+        result[node] = (left, right)
+    return result
+
+
+def find_cycle(start: str, mapping: dict[str, tuple[str, str]], directions: cycle) -> int:
+    steps = 0
+    position = start
+    while not position.endswith('Z'):
+        steps += 1
+        position = mapping[position][next(directions)]
+    return steps
+
+
+def part2(mapping: dict[str, tuple[str, str]], directions: cycle) -> int:
+    cycles = [find_cycle(key, mapping, directions) for key in mapping.keys() if key.endswith('A')]
+    return lcm(*cycles)
+
+
+_lines = read_input(True)
+_mapping = parse_map(_lines)
+print(f'Part 1: {find_cycle("AAA", _mapping, parse_directions(_lines))}')
+print(f'Part 2: {part2(_mapping, parse_directions(_lines))}')
