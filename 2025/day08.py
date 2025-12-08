39eb9d0840713952b8a496add03b37a8c5bf67e7
Tjeri <tjeri92@gmail.com>
2025-12-08 15:40:26 +0100

    minor refactor

--- 2025/day08.py
+++ 2025/day08.py
@@ -18,56 +18,40 @@ def calc_distances(junctions: list[Point3D]) -> list[tuple[float, Point3D, Point
     return distances
 
 
-def calc_clusters(connections: list[tuple[Point3D, Point3D]]) -> list[int]:
-    clusters: list[set[Point3D]] = []
-    for a, b in connections:
-        for cluster in clusters:
-            if a in cluster or b in cluster:
-                cluster.add(a)
-                cluster.add(b)
-                break
-        else:
-            clusters.append({a, b})
-
-    while True:
-        changed = False
-        for i, cluster in enumerate(clusters):
+def add_to_cluster(connection: tuple[Point3D, Point3D], clusters: list[set[Point3D]]) -> None:
+    for i, cluster in enumerate(clusters):
+        if any(junction in cluster for junction in connection):
+            cluster.update(connection)
             for j, other in enumerate(clusters[i + 1:]):
-                if cluster & other:
+                if any(junction in other for junction in connection):
                     cluster.update(other)
                     clusters.pop(i + j + 1)
-                    changed = True
                     break
-        if not changed:
             break
+    else:
+        clusters.append({*connection})
+
+
+def part1(connections: list[tuple[Point3D, Point3D]]) -> int:
+    clusters: list[set[Point3D]] = []
+    for connection in connections:
+        add_to_cluster(connection, clusters)
     sizes = [len(cluster) for cluster in clusters]
     sizes.sort(reverse=True)
-    return sizes
+    return reduce(operator.mul, sizes[:3])
 
 
 def part2(connections: list[tuple[Point3D, Point3D]], junctions: int) -> int:
     clusters: list[set[Point3D]] = []
-    for a, b in connections:
-        for i, cluster in enumerate(clusters):
-            if a in cluster or b in cluster:
-                cluster.add(a)
-                cluster.add(b)
-                for j, other in enumerate(clusters[i + 1:]):
-                    if a in other or b in other:
-                        cluster.update(other)
-                        clusters.pop(i + j + 1)
-                        break
-                break
-        else:
-            clusters.append({a, b})
+    for connection in connections:
+        add_to_cluster(connection, clusters)
         if len(clusters) == 1 and len(clusters[0]) == junctions:
-            return a.x * b.x
+            return connection[0].x * connection[1].x
     return -1
 
 
 _lines = read_input(True)
 _junctions = parse_junctions(_lines)
 _distances = calc_distances(_junctions)
-_sizes = calc_clusters([(a, b) for (_, a, b) in _distances[:1000]])
-print(f'Part 1: {reduce(operator.mul, _sizes[:3])}')
+print(f'Part 1: {part1([(a, b) for (_, a, b) in _distances[:1000]])}')
 print(f'Part 2: {part2([(a, b) for (_, a, b) in _distances], len(_junctions))}')
